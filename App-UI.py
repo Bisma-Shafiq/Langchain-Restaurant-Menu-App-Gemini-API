26560faf541c24ad935ece01240ee25f8439364a
Bisma Shafiq <bismashafiq26@gmail.com>
2024-12-16 13:02:45 +0500

    add langchain.helper

--- App-UI.py
+++ App-UI.py
@@ -1,17 +1,12 @@
 import streamlit as st
-
+import langchain_helper
 st.set_page_config(page_title="GenAi-Menu-Generator")
 st.title("Restaurant Menu GenAI")
 cuisine = st.sidebar.selectbox('Pick a cuisine',('Indian','Pakistani','Arabian','Italian','Mexican'))
 
-def generature_items(cuisine):
-    return{
-        'restaurant_name':"Bally Bally",
-        'menu_item' :'samosa,patti'
-    }
 
 if cuisine:
-    response = generature_items(cuisine)
+    response = langchain_helper.generature_items(cuisine)
     st.header(response['restaurant_name'])
     menu_item = response['menu_item'].split(" ,")
     st.write("Menu Items")
---
+++ langchain_helper.py
@@ -0,0 +1,5 @@
+def generature_items(cuisine):
+    return{
+        'restaurant_name':"Bally Bally",
+        'menu_item' :'samosa,patti'
+    }
