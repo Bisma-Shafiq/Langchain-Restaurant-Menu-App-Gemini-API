3a7693800796f9aeae83a9b4b0c555178177e387
Bisma Shafiq <bismashafiq26@gmail.com>
2024-12-16 13:04:40 +0500

    update code

--- App-UI.py
+++ App-UI.py
@@ -4,10 +4,9 @@ st.set_page_config(page_title="GenAi-Menu-Generator")
 st.title("Restaurant Menu GenAI")
 cuisine = st.sidebar.selectbox('Pick a cuisine',('Indian','Pakistani','Arabian','Italian','Mexican'))
 
-
 if cuisine:
     response = langchain_helper.generature_items(cuisine)
     st.header(response['restaurant_name'])
     menu_item = response['menu_item'].split(" ,")
-    st.write("Menu Items")
+    st.write("## Menu Items## ")
 
